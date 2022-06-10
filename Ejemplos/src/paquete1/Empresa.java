3ceefea32a340c6bc94c80c92c0a72af3b2d9980
Danf57 <danser2002@hotmail.com>
2022-06-10 11:28:26 -0500

    Repaso

--- Ejemplos/src/paquete1/Empresa.java
+++ Ejemplos/src/paquete1/Empresa.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete1;
 
 /**
@@ -10,23 +5,57 @@ package paquete1;
  * @author reroes
  */
 public class Empresa {
+
     private String nombre;
-    private Edificio [] edificios;
+    private Edificio[] edificios;
     private double costoBienesInmuebles;
-    
-    public void establecerNombre(String m){
+
+    public void establecerNombre(String m) {
         nombre = m;
     }
-    
-    public void establecerEdificios(Edificio[] m){
+
+    public void establecerEdificios(Edificio[] m) {
         edificios = m;
     }
-    
-    public String obtenerNombre(){
+
+    public String obtenerNombre() {
         return nombre;
     }
-    
-    public Edificio[] obtenerEdificios(){
+
+    public Edificio[] obtenerEdificios() {
         return edificios;
+    }
+
+    public void establecerCostoBienesInmuebles() {
+        for (Edificio edificio : edificios) {
+            costoBienesInmuebles += edificio.obtenerCosto();
+        }
+        /*
+        double suma = 0;
+        for (int i = 0 < obtenerEdificios().length; i++){
+            suma = suma + obtenerEdificios()[i].obtenerCosto();
+        }
+        costoBienesInmuebles = suma;
+        */
+    }
+    
+    public double obtenerCostoBienesInmuebles(){
+        return costoBienesInmuebles;
+    }
+
+    @Override
+    public String toString() {
+        String cadena = String.format("Nombre: %s\nLista de Edificio: ",
+                nombre);//obtenerNombre()
+        
+        for (int i = 0; i < edificios.length; i++) {
+            Edificio e = edificios[i];
+            cadena = String.format("%s\n%d. %s",
+                    cadena, i+1, e);
+        }
+        cadena = String.format("%s\nTotal de Inmuebles: %.2f\n",
+                 cadena, costoBienesInmuebles);
+
+        return cadena;
     }
 }
--- Ejemplos/src/paquete2/Ejecutor.java
+++ Ejemplos/src/paquete2/Ejecutor.java
@@ -4,20 +4,45 @@
  * and open the template in the editor.
  */
 package paquete2;
+// import paquete1.*;
+
+import paquete1.Edificio;
+import paquete1.Empresa;
 
 /**
  *
  * @author reroes
  */
 public class Ejecutor {
+
     public static void main(String[] args) {
-        
+
         // Crear una arreglo de 4 elementos de tipo Edificio
-        
+        Edificio edf1 = new Edificio("edificio central");
+        edf1.establecerCostos(10000);
+        //System.out.println(edf1);
+        Edificio edf2 = new Edificio("edificio sur");
+        edf2.establecerCostos(20000);
+
+        Edificio edf3 = new Edificio("edificio norte");
+        edf3.establecerCostos(30000);
+
+        Edificio edf4 = new Edificio("edificio secundario");
+        edf4.establecerCostos(20000);
+
+        Edificio[] lista = {edf1, edf2, edf3, edf4};
         // Crear un objeto de tipo Empresa:
         //  Nombre: Empresa de Hojas
         //  Lista de edificio: edf1, edf2, edf3, edf4
-        
+
+        Empresa empresa = new Empresa();
+
+        empresa.establecerNombre("Empresa de Hojas");
+        empresa.establecerEdificios(lista);
+        empresa.establecerCostoBienesInmuebles();
+
         // Imprimir el costo de los bienes inmuebles de la empresa
+        System.out.printf("%s",empresa);
+
     }
 }
